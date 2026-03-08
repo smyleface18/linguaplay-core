b74dbdff283ad792c45aacd69228961945dd66c4
Caleb <ca30850@gmail.com>
2026-03-07 21:01:47 -0500

    firts commit package core linguaplay

---
+++ config/eslint.config.mjs
@@ -0,0 +1,30 @@
+import js from '@eslint/js'
+import tseslint from 'typescript-eslint'
+
+export default [
+
+    js.configs.recommended,
+
+    ...tseslint.configs.recommended,
+
+    {
+        files: ['**/*.ts', '**/*.tsx'],
+
+        languageOptions: {
+            parser: tseslint.parser,
+        },
+
+        rules: {
+            '@typescript-eslint/no-unused-vars': [
+                'warn',
+                { argsIgnorePattern: '^_' },
+            ],
+
+            '@typescript-eslint/explicit-function-return-type': 'off',
+
+            '@typescript-eslint/no-explicit-any': 'warn',
+
+            'no-console': 'warn',
+        },
+    },
+]
---
+++ config/prettier.config.js
@@ -0,0 +1,18 @@
+/** @type {import("prettier").Config} */
+
+module.exports = {
+    semi: true,
+    singleQuote: true,
+
+    trailingComma: 'all',
+
+    printWidth: 90,
+
+    tabWidth: 2,
+
+    arrowParens: 'always',
+
+    bracketSpacing: true,
+
+    endOfLine: 'lf',
+}
